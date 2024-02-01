f74fbce6c7bea89a464abe8ad4cbb936d2415a47
hgh <1677498109@qq.com>
2024-02-01 09:59:54 +0800

    路径更新

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const files = [
-  { id: 1, name: 'JVM.md' },
-  { id: 2, name: 'RabbitMQ.md' },
-  { id: 3, name: '设计模式.md' },
+  { id: 1, name: 'md/JVM.md' },
+  { id: 2, name: 'md/RabbitMQ.md' },
+  { id: 3, name: 'md/设计模式.md' },
 ]
