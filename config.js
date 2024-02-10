7ff5b7ae1dd16fc964b157e3b2293720592ac26a
noodels <1677498109@qq.com>
2024-02-10 17:40:45 +0800

    remove mysql

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const files = [
   { id: 1, name: 'JVM', path: 'md/JVM.md' },
-  { id: 2, name: 'MySQL', path: 'md/MySQL.md' },
+  // { id: 2, name: 'MySQL', path: 'md/MySQL.md' },
   { id: 3, name: 'Netty', path: 'md/Netty.md' },
   { id: 4, name: 'RabbitMQ', path: 'md/RabbitMQ.md' },
   { id: 5, name: 'Redis', path: 'md/Redis.md' },
